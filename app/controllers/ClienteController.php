94b34d178a6580ffed3512459a90233fb3998507
Brena Almeida <brenaalmeida314@gmail.com>
2017-12-03 22:30:33 -0200

    atualização
    Alterando a página da aplicação.

--- app/controllers/ClienteController.php
+++ app/controllers/ClienteController.php
@@ -6,7 +6,7 @@ namespace app\controllers;
 class ClienteController extends Controller {
     
     public function viewLogin() {
-        echo $this->twig->render('cliente.html.twig', []);
+        echo $this->twig->render('loginCliente.html.twig', []);
     }
     
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
      $app->get('/cliente', 'app\controllers\ClienteController:viewLogin')->setName('cliente-login');
      $app->get('/funcionario', 'app\controllers\FuncionarioController:viewLogin')->setName('funcionario-login');
      $app->get('/dash', 'app\controllers\DashController:view')->setName('dash');
-     $app->get('/fornecedor', 'app\controllers\FornecedorController:view')->setName('fornecedor');
+     $app->get('/cadastro-fornecedor', 'app\controllers\FornecedorController:view')->setName('fornecedor');
      $app->get('/cadastro-funcionario', 'app\controllers\FuncionarioController:viewCadastroF')->setName('cadastro-funcionario');
      $app->get('/cadastro-cliente', 'app\controllers\FuncionarioController:viewCadastroC')->setName('cadastro-cliente');
      
